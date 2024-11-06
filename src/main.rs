5d280ef65d8e81059f92d162e4cf69bb98edfea6
humble-wolf <paulfarkas0707@gmail.com>
2024-11-06 04:09:13 +0100

    feat: add basic get and post api

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 // import Rocket
 #[macro_use] extern crate rocket;
 
-// this is our get route which will be requested at the "/" location wherever it is mounted
-#[get("/")]
-fn say_hello() -> &'static str {
-  "Hello, welcome to the api!"
-}
+// add our routes and services modules
+mod routes;
+mod services;
+
+// import our routes
+use routes::date::get_current_date;
+use routes::date::date_plus_month;
 
 // start the web server and mount our get route at "/api". Can replace /api with anything
 // or just leave it as "/" as the default location
 #[launch]
 fn rocket() -> _ {
-  rocket::build().mount("/api", routes![say_hello])
+    rocket::build().mount("/api", routes![get_current_date, date_plus_month])
 }
